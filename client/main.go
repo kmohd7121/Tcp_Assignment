9bb295ae68113fb5a5918896e568d024e0469c78
kmohd7121 <65274900+kmohd7121@users.noreply.github.com>
2021-02-04 14:20:33 +0530

    Add files via upload

--- client/main.go
+++ client/main.go
@@ -1,1 +1,38 @@
+package main
 
+import (
+	"net"
+	"fmt"
+	"log"
+)
+func main(){
+	conn,err:=net.Dial("tcp","localhost:3000")
+	//string(data)
+	if err!=nil {
+		log.Fatalln(err)
+	}
+
+	_ , err = conn.Write([]byte("hello server"))
+	if err!=nil {
+		log.Fatalln(err)
+	}
+	fmt.Println("Message sent: hello server")
+
+	_,err = conn.Write([]byte("how are you"))
+	if err!=nil {
+		log.Fatalln(err)
+	}
+	fmt.Println("Message sent: how are you")
+
+	for {
+		buffer:=make([]byte,1400)
+		dataSize,err:=conn.Read(buffer)
+		if err!=nil{
+			fmt.Println("connection close")
+			return
+		}
+
+		data:=buffer[:dataSize]
+		fmt.Println("received message:",string(data))
+	}
+}

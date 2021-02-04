ae438bbbb185267e4bb8c8dacfd7f40cc1dd90e6
kmohd7121 <65274900+kmohd7121@users.noreply.github.com>
2021-02-04 14:22:49 +0530

    Add files via upload

--- server/main.go
+++ server/main.go
@@ -1,1 +1,45 @@
+package main
 
+import(
+	"fmt"
+	"log"
+	"net"
+)
+
+func main(){
+	fmt.Println("server listening on 3000")
+	listener,err:=net.Listen("tcp","localhost:3000")
+	if err!=nil {
+		log.Fatalln(err)
+	}
+	defer listener.Close()
+
+	for {
+		conn,err:=listener.Accept()
+		if err!=nil{
+			log.Fatalln(err)
+		}
+		fmt.Println("new connection")
+		go listenConnection(conn)
+	}
+}
+
+func listenConnection(conn net.Conn) {
+	for{
+		buffer:=make([]byte,1400)
+		dataSize,err:=conn.Read(buffer)
+		if err!=nil{
+			fmt.Println("connection close")
+			return
+		}
+
+		data:=buffer[:dataSize]
+		fmt.Println("received message:",string(data))
+
+		_,err=conn.Write(data)
+		if err!=nil{
+			log.Fatalln(err)
+		}
+		fmt.Println("Message sent:",string(data))
+	}
+}
